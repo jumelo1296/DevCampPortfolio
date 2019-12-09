9340398c8e862d44ed54c0fa012010ddd2585ee8
Hector Melo <hectormeloc@gmail.com>
2019-12-08 19:07:02 -0500

    Integrated new and create actions on Portofolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,32 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items=Portfolio.all
 	end
+
+	def new
+		@portfolio_items=Portfolio.new
+	end
+
+	
+
+	def create
+    @portfolio_items = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
+
+private
+    # Use callbacks to share common setup or constraints between actions.
+    
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end

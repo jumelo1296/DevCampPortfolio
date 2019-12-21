ad53ec7d041faf8b863085facfb9065639131946
Hector Melo <hectormeloc@gmail.com>
2019-12-20 21:28:05 -0500

    Implemented Concern in Portfolio and Skill Model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title,:body, :main_image, :thumb_image
 def self.angular
 	where(subtitle: 'Angular')
@@ -6,12 +7,21 @@ end
 
 scope :ruby_on_rails_items, ->  {where(subtitle: 'Ruby on Rails')}
 
-#to set default values
+#to set default values after .new method is called in Portfolio controller
 after_initialize :set_defaults
 
+#def set_defaults
+#	# ||= set values only if current value is nil.
+#	self.main_image ||="http://placehold.it/600x400"
+#	self.thumb_image ||="http://placehold.it/350x200"
+#end
+
+#subtituting the method above with "Placeholder"; Use of Concern
+
 def set_defaults
-	self.main_image ||="http://placehold.it/600x400"
-	self.thumb_image ||="http://placehold.it/350x200"
+	self.main_image ||=Placeholder.image_generator(height:'600', width:'400')
+	self.thumb_image ||=Placeholder.image_generator(height:'350', width:'200')
 end
+
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,21 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title
+
+after_initialize :set_defaults
+
+#def set_defaults
+#	# ||= set values only if current value is nil.
+#	self.badge ||="http://placehold.it/250x250"
+#end
+
+
+# Rplacing the Method above for setting default with the use of "Placeholder" Concern
+def set_defaults
+	# ||= set values only if current value is nil.
+	self.badge ||=Placeholder.image_generator(height:'250', width:'250')
+end
+
+
+
 end
